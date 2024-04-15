358a20a45f49cd8a56277bdb3cfa3453a8d084d4
Cam DeSousa <134101013+bluecrow34@users.noreply.github.com>
2024-04-15 16:20:10 -0400

    Create auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,72 @@
+"use strict";
+
+/** Convenience middleware to handle common auth cases in routes. */
+
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config");
+const { UnauthorizedError } = require("../expressError");
+
+
+/** Middleware: Authenticate user.
+ *
+ * If a token was provided, verify it, and, if valid, store the token payload
+ * on res.locals (this will include the username and isAdmin field.)
+ *
+ * It's not an error if no token was provided or if the token is not valid.
+ */
+
+function authenticateJWT(req, res, next) {
+  try {
+    const authHeader = req.headers && req.headers.authorization;
+    if (authHeader) {
+      const token = authHeader.replace(/^[Bb]earer /, "").trim();
+      res.locals.user = jwt.verify(token, SECRET_KEY);
+    }
+    return next();
+  } catch (err) {
+    return next();
+  }
+}
+
+/** Middleware to use when they must be logged in.
+ *
+ * If not, raises Unauthorized.
+ */
+
+function ensureLoggedIn(req, res, next) {
+  try {
+    if (!res.locals.user) throw new UnauthorizedError();
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
+
+
+// Check Authorization for Companies and Users
+
+
+function authorizeAdmin(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Authorization header missing' });
+  }
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, 'your-secret-key', (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Invalid token' });
+    }
+    if (!decoded.is_admin) {
+      return res.status(403).json({ error: 'Unauthorized: User is not an admin' });
+    }
+    next();
+  });
+}
+
+
+module.exports = {
+  authenticateJWT,
+  ensureLoggedIn,
+  authorizeAdmin,
+};
